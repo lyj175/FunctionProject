5bc8f64e287e0f4d74cd80c0a35bb5532c4d6f8d
lyj <m15902505484@163.com>
2018-09-10 18:06:36 +0800

    test

--- app/src/main/java/com/example/administrator/function/HostActivity.java
+++ app/src/main/java/com/example/administrator/function/HostActivity.java
@@ -1,11 +1,20 @@
 package com.example.administrator.function;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import cn.kevin.floatingeditor.EditorCallback;
 import cn.kevin.floatingeditor.EditorHolder;
@@ -13,17 +22,21 @@ import cn.kevin.floatingeditor.FloatEditorActivity;
 
 public class HostActivity extends AppCompatActivity {
     private final int REQUEST_VIDEO_CODE = 1;
+    private static final String TAG = "HostActivity";
 
     private Button mShare;
     private Button mDynamic;
     private Button mSelectVideo;
+    private ImageView mTestImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
-        mShare = (Button)findViewById(R.id.share);
-        mDynamic = (Button)findViewById(R.id.dynamic);
-        mSelectVideo = (Button)findViewById(R.id.select_video);
+        mShare = (Button) findViewById(R.id.share);
+        mDynamic = (Button) findViewById(R.id.dynamic);
+        mSelectVideo = (Button) findViewById(R.id.select_video);
+        mTestImage = (ImageView) findViewById(R.id.testImage);
 
         mShare.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,7 +45,7 @@ public class HostActivity extends AppCompatActivity {
 //                intent.setType("text/plain");
 //                intent.putExtra(Intent.EXTRA_TEXT, "分享的内容");
 //                startActivity(Intent.createChooser(intent,"选择应用"));
-                EditorCallback editorCallback = new EditorCallback(){
+                EditorCallback editorCallback = new EditorCallback() {
 
                     @Override
                     public void onCancel() {
@@ -44,9 +57,10 @@ public class HostActivity extends AppCompatActivity {
                         //onSubmit callback
                         Intent intent = new Intent(Intent.ACTION_SEND);
                         intent.setType("text/plain");
-                        intent.putExtra(Intent.EXTRA_TEXT , content);
+                        intent.putExtra(Intent.EXTRA_TEXT, content);
                         startActivity(Intent.createChooser(intent, "发送给谁？"));
                     }
+
                     @Override
                     public void onAttached(ViewGroup rootView) {
                         //on layout Attached callback
@@ -63,15 +77,20 @@ public class HostActivity extends AppCompatActivity {
         mDynamic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(HostActivity.this , com.zq.weixinselectpicture.MainActivity.class));
+                startActivity(new Intent(HostActivity.this, com.zq.weixinselectpicture.MainActivity.class));
             }
         });
         mSelectVideo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent, REQUEST_VIDEO_CODE);
+                Intent intentPic = new Intent(
+                        Intent.ACTION_GET_CONTENT);
+                intentPic.addCategory(Intent.CATEGORY_OPENABLE);
+                intentPic.setType("video/*;image/*");
+                startActivityForResult(intentPic,
+                        REQUEST_VIDEO_CODE);
             }
         });
     }
+
 }
